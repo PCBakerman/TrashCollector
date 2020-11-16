f314aa0d5a64290b47cdbe00b892065877bc5531
PCBakerman <72618089+PCBakerman@users.noreply.github.com>
2020-11-16 11:57:18 -0600

    First migration, and added Employee Model and Customer Model.

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+   
+    public class Customer
+    {   [Key]
+        public int CustomerId { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public char CustomerAddress { get; set; }
+        public int CustomerPhoneNumber { get; set; }
+        public char CustomerEmailAddress { get; set; }
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {   [Key]
+        public int EmployeeId { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public char EmployeeAddress { get; set; }
+        public int EmployeePhoneNumber { get; set; }
+        public char EmployeeEmailAddress { get; set; }
+    }
+}
