9ba453fc4539d47c4fb6d30ff2689db1a9782e2b
PCBakerman <72618089+PCBakerman@users.noreply.github.com>
2020-11-17 21:01:09 -0600

    Added Location Model.

---
+++ TrashCollector/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TrashCollector.Models
+{
+    public class Location
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public IdentityUser User { get; set; }
+    }
+}
