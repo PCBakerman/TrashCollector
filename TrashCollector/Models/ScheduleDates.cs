353e158536b2a802229932240e0d71e760d679bd
PCBakerman <72618089+PCBakerman@users.noreply.github.com>
2020-11-17 22:54:13 -0600

    Added ScheduleDates model.

---
+++ TrashCollector/Models/ScheduleDates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class ScheduleDates
+    {
+        [Key]
+        public int ScheduleDateId { get; set; }
+        public DateTime Date { get; set; }
+        public string LocationId { get; set; }
+        [ForeignKey("LocationId")]
+        public Location Location { get; set; }
+        public string EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+
+    }
+}
